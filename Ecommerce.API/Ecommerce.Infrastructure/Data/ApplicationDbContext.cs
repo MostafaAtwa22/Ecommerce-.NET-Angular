47914ff2b1f3cf6f9fa07faca217c22af58e778c
MostafaAtwa <mostafajobs22@gmail.com>
2025-10-19 23:26:44 +0300

    (feat): Use clean arch and start creating entities & context with seeding data

---
+++ Ecommerce.API/Ecommerce.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+using Ecommerce.Core.Entities;
+using Ecommerce.Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
